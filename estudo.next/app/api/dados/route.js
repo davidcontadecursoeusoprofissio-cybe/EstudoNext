115843199b838f9e1a03609ee5374112ee4a9f4a
davidcontadecursoeusoprofissio-cybe <davidcontadecursoeusoprofissio@gmail.com>
2026-09-23 13:09:37 -0300

    atualiza

--- estudo.next/app/api/dados/route.js
+++ estudo.next/app/api/dados/route.js
@@ -1,5 +1,5 @@
 import {NextResponse} from "next/server";
-import sqline3 from 'sqlite3'
+import sqlite3 from 'sqlite3'
 import {open} from 'sqlite'
 import path from "path"
 
@@ -10,28 +10,29 @@ async function abrirBanco(){
     });
     await db.exec(`
         CREATE TABLE IF NOT EXISTS dados(
-        id INTEGER PRIMARY KEY AUTOINCREMENT
-
-        nome TEXT NOT NULL,
-        email TEXT NOT NULL,
-        senha INTEGER NOT NULL,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            email TEXT NOT NULL,
+            senha INTEGER NOT NULL
         )
-        `);
+    `);
+    return db;
+}
 
-        export async function GET(){
-            const db = await abrirBanco();
-            const usuarios = await db.all('SELECT * FROM dados')
-            return NextResponse.json(usuarios)
-        }
-        export async function POST(request){
-            const dado = await request.json();
-            const { nome, email, senha } = dado;
-            const db = await abrirBanco();
+export async function GET(){
+    const db = await abrirBanco();
+    const usuarios = await db.all('SELECT * FROM dados');
+    return NextResponse.json(usuarios);
+}
 
-            const resultado = await db.run(
-                `INSERT INTO dados (nome, email, senha)VALUES(?,?,?)`,
-                [nome, email, senha ?? null]
-            );
-            return NextResponse.json({ mensagem: "Cadastro feito com sucesso"}, {status: 201})
-        }
+export async function POST(request){
+    const dado = await request.json();
+    const { nome, email, senha } = dado;
+    const db = await abrirBanco();
+
+    const resultado = await db.run(
+        `INSERT INTO dados (nome, email, senha) VALUES (?, ?, ?)`,
+        [nome, email, senha ?? null]
+    );
+    return NextResponse.json({ mensagem: "Cadastro feito com sucesso"}, {status: 201});
 }
